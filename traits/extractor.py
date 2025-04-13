d14b48f38c6e0184150b9723b66d220edd290cc6
OAOAaA <ojch1121@gmail.com>
2025-04-13 22:30:07 +0800

    🐞 修正 zodiac 相容性判斷缺少 b_birth 的參數

--- traits/extractor.py
+++ traits/extractor.py
@@ -1,13 +1,15 @@
-from modules.zodiac import get_zodiac_compatibility
-from modules.tarot import get_tarot_compatibility
-from modules.wuxing import get_wuxing_interaction
-from modules.ziwei import get_ziwei_info
-from modules.yijing import get_yijing_hexagram
-from modules.numerology import get_numerology_info
-from modules.suggestion import get_pairing_suggestion
+from ..modules.zodiac import get_zodiac_compatibility
+from ..modules.tarot import get_tarot_compatibility
+from ..modules.wuxing import get_wuxing_interaction
+from ..modules.ziwei import get_ziwei_info
+from ..modules.yijing import get_yijing_hexagram
+from ..modules.numerology import get_numerology_info
+from ..modules.suggestion import get_pairing_suggestion
 
 def extract_traits(birth: str) -> dict:
     return {
+def extract_pair_traits(a_birth: str, b_birth: str) -> dict:
+    traits = {
         "birth": birth,
         "zodiac": get_zodiac_compatibility(birth),
         "tarot": get_tarot_compatibility(birth),
