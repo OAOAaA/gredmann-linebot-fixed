b8a116b9df8a6c04bd6dfcbf6a9e981ba279035c
OAOAaA <ojch1121@gmail.com>
2025-04-13 22:35:41 +0800

    🛠 修正 extractor.py 語法錯誤：補上未關閉的大括號

--- traits/extractor.py
+++ traits/extractor.py
@@ -1,13 +1,11 @@
-from ..modules.zodiac import get_zodiac_compatibility
-from ..modules.tarot import get_tarot_compatibility
-from ..modules.wuxing import get_wuxing_interaction
-from ..modules.ziwei import get_ziwei_info
-from ..modules.yijing import get_yijing_hexagram
-from ..modules.numerology import get_numerology_info
-from ..modules.suggestion import get_pairing_suggestion
+from modules.zodiac import get_zodiac_compatibility
+from modules.tarot import get_tarot_compatibility
+from modules.wuxing import get_wuxing_interaction
+from modules.ziwei import get_ziwei_info
+from modules.yijing import get_yijing_hexagram
+from modules.numerology import get_numerology_info
+from modules.suggestion import get_suggestion
 
-def extract_traits(birth: str) -> dict:
-    return {
 def extract_pair_traits(a_birth: str, b_birth: str) -> dict:
     return {
         "zodiac_result": get_zodiac_compatibility(a_birth, b_birth),
@@ -17,4 +15,4 @@ def extract_pair_traits(a_birth: str, b_birth: str) -> dict:
         "yijing_result": get_yijing_hexagram(a_birth),
         "numerology_result": get_numerology_info(a_birth),
         "suggestion": get_suggestion(a_birth, b_birth),
-    }
+    }  # ✅ ← 結尾的大括號絕對不能漏
